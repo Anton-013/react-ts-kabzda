17c54fdc51e371019f29ba61c317307c2ff1726a
Anton-013 <antonchuvashovoren@mail.ru>
2025-05-21 16:03:17 +0300

    story Accordion

---
+++ src/components/Accordion/Accordion.stories.tsx
@@ -0,0 +1,43 @@
+import type { Meta } from '@storybook/react'
+import { action } from '@storybook/addon-action'
+
+import { Accordion } from './Accordion'
+import { useState } from 'react'
+
+const meta: Meta<typeof Accordion> = {
+    component: Accordion,
+}
+
+export default meta
+
+
+
+export const CollapsedAccordion = () => {
+    return (
+        <Accordion
+            title='CollapsedAccordion'
+            collapsed={true}
+            onClick={() => { }} />
+    )
+}
+
+export const OpenedAccordion = () => {
+    return (
+        <Accordion
+            title='OpenedAccordion'
+            collapsed={false}
+            onClick={() => { }} />
+    )
+}
+
+export const AccordionDemo = () => {
+    const [collapsed, setCollapsed] = useState(false)
+    return (
+        <Accordion
+            title='Accordion'
+            collapsed={collapsed}
+            onClick={() => {
+                setCollapsed(!collapsed)
+            }} />
+    )
+}
